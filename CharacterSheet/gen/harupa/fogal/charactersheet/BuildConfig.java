9b01cbfdb8e77a0e89011315b24cd97a5807479e
Davebotextreme <davebotextreme@gmail.com>
2013-12-04 12:50:05 -0500

    Added Java Files that Brian Wrote

---
+++ CharacterSheet/gen/harupa/fogal/charactersheet/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package harupa.fogal.charactersheet;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CharacterSheet/src/harupa/fogal/charactersheet/D20Class.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class D20Class
+{
+	private String name;
+	private int hitDie;
+	private boolean classSkill[] = new boolean[38];
+	private LinkedList<SpellAbility> spellLikeAbility;
+	private int level;
+}
